a446192453822b047c0edc8913505435efcd5555
Vuong Manh Nghia <vuongmanhnghia@gmail.com>
2024-08-14 08:50:20 +0700

    commit DOM_03

---
+++ DOM/BAI_03/script.js
@@ -0,0 +1,6 @@
+let list = document.getElementById("list");
+
+// Thêm phần tử vào cuối danh sách
+let newItem = document.createElement("li");
+newItem.innerHTML = "Item 3";
+list.appendChild(newItem);
